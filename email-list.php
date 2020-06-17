340b3fd29959d17475e716ac2ce862f3691e6e6c
deaddandilion <66956287+deaddandilion@users.noreply.github.com>
2020-06-17 18:07:42 -0500

    Delete email-list.php

--- email-list.php
+++
@@ -1,9 +0,0 @@
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>e_</title>
-  </head>
-  <body>
-    <p>welcome. you now have access to our discord. please do not look at the email list unless permission is given. that will result in a ban.</p>
-  </body>
-</html>
